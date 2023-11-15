b08bc2974a3cbe90a6d5a8e11864cfceeb86c5e7
Joe di Stefano <joeydi@okaypl.us>
2023-11-15 10:48:50 -0500

    update page title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>RGS Website Comps</title>
+                <title>American Plating Power Website Comps</title>
                 <meta name="description" content="" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
