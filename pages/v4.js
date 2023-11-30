b98197fe428064d91ae3267de0a88441a5649c1f
Joe di Stefano <joeydi@okaypl.us>
2023-11-29 23:10:48 -0500

    add new homepage versions

---
+++ pages/v4.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Header from "@/components/Header";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>American Plating Power Website Comps</title>
+                <meta name="description" content="" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <main>
+                <img src="homepage-v17.png" width="3840" height="7820" alt="" />
+            </main>
+        </>
+    );
+}
